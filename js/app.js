7c8d3cbe9f43ea1b0b9e4919b4b82103502d6207
Adrienne <adrie.k@gmail.com>
2015-02-03 00:11:55 -0800

    Move fruit and veggies around.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,36 @@
-/* setup your angular app here */
+var foodApp = angular.module('FoodApp', []);
 
-//debug stuff to show the app is loading and fruit / veggies are available
-console.log('App Started');
-console.log('Fruit count', fruit.length);
-console.log('Veggie count', vegetables.length);
+foodApp.run(function(){
+  console.log('App Started');
+});
+
+function randomize(){
+  return (Math.round(Math.random())-0.5);
+}
+
+foodApp.controller('FoodApp',['$scope', function($scope){
+  $scope.fruit = []
+  $scope.vegetables = []
+  $scope.allFoods = fruit.concat(vegetables).sort(randomize)
+
+  $scope.addFruit = function(currentIndex){
+    $scope.fruit.unshift({name:$scope.allFoods[currentIndex]})
+    $scope.allFoods.splice([currentIndex],1)
+  }
+
+  $scope.addVegetables = function(currentIndex){
+    $scope.vegetables.unshift({name:$scope.allFoods[currentIndex]})
+    $scope.allFoods.splice([currentIndex],1)
+  }
+
+  $scope.returnFruit = function(currentIndex){
+    $scope.allFoods.unshift($scope.fruit[currentIndex].name)
+    $scope.fruit.splice([currentIndex],1)
+  }
+
+  $scope.returnVegetables = function(currentIndex){
+    $scope.allFoods.unshift($scope.vegetables[currentIndex].name)
+    $scope.vegetables.splice([currentIndex],1)
+  }
+}]);
+
